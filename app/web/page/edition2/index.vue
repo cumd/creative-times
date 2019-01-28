e0a45602db1235e2281b0d084b5f9ed5f79397c8
ZhangKaitlyn <1873070425@qq.com>
2019-01-28 14:38:55 +0800

    NPL报名

--- app/web/page/edition2/index.vue
+++ app/web/page/edition2/index.vue
@@ -3,6 +3,7 @@
     <code-banner></code-banner>
     <page-guide></page-guide>
     <game-intro></game-intro>
+    <code-day class="edition2-page-offset"></code-day>
   </div>
 </template>
 
@@ -14,12 +15,23 @@
 import CodeBanner from '@/component/edition2/CodeBanner'
 import PageGuide from '@/component/edition2/PageGuide'
 import GameIntro from '@/component/edition2/GameIntro'
+import CodeDay from '@/component/edition2/CodeDay'
 export default {
   components: {
     CodeBanner,
     PageGuide,
-    GameIntro
+    GameIntro,
+    CodeDay
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.edition2-page {
+  &-offset /deep/ .code-day-container {
+    position: relative;
+    top: -80px;
+  }
+}
+</style>
 
