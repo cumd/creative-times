09639822426ef3f425c7e5e58833ff63b2f17343
ZhangKaitlyn <1873070425@qq.com>
2019-01-28 11:39:27 +0800

    大赛简介

--- app/web/page/edition2/index.vue
+++ app/web/page/edition2/index.vue
@@ -2,6 +2,7 @@
   <div class="edition2-page">
     <code-banner></code-banner>
     <page-guide></page-guide>
+    <game-intro></game-intro>
   </div>
 </template>
 
@@ -12,10 +13,12 @@
 <script>
 import CodeBanner from '@/component/edition2/CodeBanner'
 import PageGuide from '@/component/edition2/PageGuide'
+import GameIntro from '@/component/edition2/GameIntro'
 export default {
   components: {
     CodeBanner,
-    PageGuide
+    PageGuide,
+    GameIntro
   }
 }
 </script>
